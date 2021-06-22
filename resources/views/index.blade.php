8a7471c0b9ddafa19dd2012954a588422b7bd69d
Ansh16012000 <anshverma242@gmail.com>
2021-06-22 14:56:47 +0530

    updated index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,38 +38,8 @@
 <div class="topbar">
 <div class="container">
 <div class="row align-items-center">
-<div class="col-lg-4 col-md-4 col-12">
-<div class="top-left">
-<ul class="menu-top-link">
-<li>
-<div class="select-position">
-<select id="select4">
-<option value="0" selected>$ USD</option>
-<option value="1">€ EURO</option>
-<option value="2">$ CAD</option>
-<option value="3">₹ INR</option>
-<option value="4">¥ CNY</option>
-<option value="5">৳ BDT</option>
-</select>
-</div>
-</li>
-<li>
-<div class="select-position">
-<select id="select5">
-<option value="0" selected>English</option>
-<option value="1">Español</option>
-<option value="2">Filipino</option>
-<option value="3">Français</option>
-<option value="4">العربية</option>
-<option value="5">हिन्दी</option>
-<option value="6">বাংলা</option>
-</select>
-</div>
-</li>
-</ul>
-</div>
-</div>
-<div class="col-lg-4 col-md-4 col-12">
+
+<div class="col-lg-8 col-md-8 col-12">
 <div class="top-middle">
 <ul class="useful-links">
 <li><a href="#">Home</a></li>
@@ -113,19 +83,7 @@ Hello
 
 <div class="main-menu-search">
 
-<div class="navbar-search search-style-5">
-<div class="search-select">
-<div class="select-position">
-<select id="select1">
-<option selected>All</option>
-<option value="1">option 01</option>
-<option value="2">option 02</option>
-<option value="3">option 03</option>
-<option value="4">option 04</option>
-<option value="5">option 05</option>
-</select>
-</div>
-</div>
+<div class="navbar-search">
 <div class="search-input">
 <input type="text" placeholder="Search">
 </div>
@@ -137,12 +95,12 @@ Hello
 </div>
 
 </div>
-<div class="col-lg-4 col-md-2 col-5">
+<!-- <div class="col-lg-4 col-md-2 col-5">
 <div class="middle-right-area">
 <div class="nav-hotline">
 <i class="lni lni-phone"></i>
 <h3>Hotline:
-<span>(+100) 123 456 7890</span>
+<span>(+91) 9974075015 </span>
 </h3>
 </div>
 <div class="navbar-cart">
@@ -200,11 +158,11 @@ Apple Watch Series 6</a></h4>
 </div>
 </div>
 </div>
+</div> -->
 </div>
 </div>
 </div>
-</div>
-
+<!-------------------------------------------------------------------------------------------------------------------------->
 
 <div class="container">
 <div class="row align-items-center">
@@ -214,8 +172,10 @@ Apple Watch Series 6</a></h4>
 <div class="mega-category-menu">
 <span class="cat-button"><i class="lni lni-menu"></i>All Categories</span>
 <ul class="sub-category">
-<li><a href="product-grids.html">Electronics <i class="lni lni-chevron-right"></i></a>
-<ul class="inner-sub-category">
+<li><a href="product-grids.html">Electronics 
+<!-- <i class="lni lni-chevron-right"></i> -->
+</a>
+<!-- <ul class="inner-sub-category">
 <li><a href="product-grids.html">Digital Cameras</a></li>
 <li><a href="product-grids.html">Camcorders</a></li>
 <li><a href="product-grids.html">Camera Drones</a></li>
@@ -226,12 +186,12 @@ Apple Watch Series 6</a></h4>
 <li><a href="product-grids.html">Chargers</a></li>
 <li><a href="product-grids.html">Batteries</a></li>
 <li><a href="product-grids.html">Cables & Adapters</a></li>
-</ul>
+</ul> -->
 </li>
-<li><a href="product-grids.html">accessories</a></li>
-<li><a href="product-grids.html">Televisions</a></li>
-<li><a href="product-grids.html">best selling</a></li>
-<li><a href="product-grids.html">top 100 offer</a></li>
+<li><a href="product-grids.html">Automobiles</a></li>
+<li><a href="product-grids.html">Grocery</a></li>
+<li><a href="product-grids.html">Hardware and Plumbing</a></li>
+<!-- <li><a href="product-grids.html">top 100 offer</a></li>
 <li><a href="product-grids.html">sunglass</a></li>
 <li><a href="product-grids.html">watch</a></li>
 <li><a href="product-grids.html">man’s product</a></li>
@@ -239,6 +199,10 @@ Apple Watch Series 6</a></h4>
 <li><a href="product-grids.html">Computers & Tablets </a></li>
 <li><a href="product-grids.html">Video Games </a></li>
 <li><a href="product-grids.html">Home Appliances </a></li>
+<li><a class="dropdown-item" href="#">Automobiles</a></li>
+<li><a class="dropdown-item" href="#">Grocery</a></li>
+<li><a class="dropdown-item" href="#">Electronics</a></li>
+<li><a class="dropdown-item" href="#">Hardware and Plumbing</a></li> -->
 </ul>
 </div>
 
