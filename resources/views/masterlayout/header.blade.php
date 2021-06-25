df5fb50af91ee814151334c0b9cfe8ce5093474a
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-06-25 23:47:58 +0530

    responsive

--- resources/views/masterlayout/header.blade.php
+++ resources/views/masterlayout/header.blade.php
@@ -1,50 +1,50 @@
 <header class="header navbar-area">
 
-<div class="topbar" style="background-color:#292b2c;">
-<div class="container" >
-<div class="row align-items-center">
+    <div class="topbar" style="background-color:#292b2c;">
+        <div class="container">
+            <div class="row align-items-center">
 
-<div class="col-lg-8 col-md-8 col-12">
-<div class="top-middle">
-<ul class="useful-links">
-<li><a href="#">Home</a></li>
-<li><a href="about-us.html">About Us</a></li>
-<li><a href="contact.html">Contact Us</a></li>
-</ul>
-</div>
-</div>
-<div class="col-lg-4 col-md-4 col-12">
-<div class="top-end">
-<div class="user">
-<i class="lni lni-user"></i>
-Hello
-</div>
-<ul class="user-login">
-<li>
-<a href="login.html">Sign In</a>
-</li>
-<li>
-<a href="http://store-locator.test/register#">Register</a>
-</li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-</div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <div class="top-middle">
+                        <ul class="useful-links">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="about-us.html">About Us</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="top-end">
+                        <div class="user">
+                            <i class="lni lni-user"></i>
+                            Hello
+                        </div>
+                        <ul class="user-login">
+                            <li>
+                                <a href="login.html">Sign In</a>
+                            </li>
+                            <li>
+                                <a href="http://store-locator.test/register#">Register</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-<div class="header-middle">
-<div class="container">
-<div class="row align-items-center">
-<div class="col-lg-3 col-md-3 col-7">
+    <div class="header-middle">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-3 col-md-3 col-7">
 
-<a class="navbar-brand" href="http://store-locator.test/index">
-<img src="{{asset('images/logo/logo.svg')}}" alt="Logo">
-</a>
+                    <a class="navbar-brand" href="http://store-locator.test/index">
+                        <img src="{{asset('images/logo/logo.svg')}}" alt="Logo">
+                    </a>
 
-</div>
-<!-- <div class="col-lg-5 col-md-7 d-xs-none">
+                </div>
+                <!-- <div class="col-lg-5 col-md-7 d-xs-none">
 
 <div class="main-menu-search">
 
@@ -60,7 +60,7 @@ Hello
 </div>
 
 </div> -->
-<!-- <div class="col-lg-4 col-md-2 col-5">
+                <!-- <div class="col-lg-4 col-md-2 col-5">
 <div class="middle-right-area">
 <div class="nav-hotline">
 <i class="lni lni-phone"></i>
@@ -124,23 +124,23 @@ Apple Watch Series 6</a></h4>
 </div>
 </div>
 </div> -->
-</div>
-</div>
-</div>
-<!-------------------------------------------------------------------------------------------------------------------------->
+            </div>
+        </div>
+    </div>
+    <!-------------------------------------------------------------------------------------------------------------------------->
 
-<div class="container">
-<div class="row align-items-center">
-<div class="col-lg-8 col-md-5 col-12">
-<div class="nav-inner">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8 col-md-5 col-12">
+                <div class="nav-inner">
 
-<div class="mega-category-menu">
-<span class="cat-button"><i class="lni lni-menu"></i>Categories</span>
-<ul class="sub-category">
-<li><a href="product-grids.html" >Electronics and Electrician
-<!-- <i class="lni lni-chevron-right"></i> -->
-</a>
-<!-- <ul class="inner-sub-category">
+                    <div class="mega-category-menu">
+                        <span class="cat-button"><i class="lni lni-menu"></i>Categories</span>
+                        <ul class="sub-category">
+                            <li><a href="product-grids.html">Electronics and Electrician
+                                    <!-- <i class="lni lni-chevron-right"></i> -->
+                                </a>
+                                <!-- <ul class="inner-sub-category">
 <li><a href="product-grids.html">Digital Cameras</a></li>
 <li><a href="product-grids.html">Camcorders</a></li>
 <li><a href="product-grids.html">Camera Drones</a></li>
@@ -152,13 +152,13 @@ Apple Watch Series 6</a></h4>
 <li><a href="product-grids.html">Batteries</a></li>
 <li><a href="product-grids.html">Cables & Adapters</a></li>
 </ul> -->
-</li>
-<li><a  href="product-grids.html">Automobiles and Mechanics</a></li>
-<li><a  href="product-grids.html">Grocery</a></li>
-<li><a  href="product-grids.html">Hardware and Plumbing</a></li>
-<li><a  href="product-grids.html">Pharmacy</a></li>
-<li><a  href="product-grids.html">Furniture and Carpentry</a></li>
-<!-- <li><a href="product-grids.html">top 100 offer</a></li>
+                            </li>
+                            <li><a href="product-grids.html">Automobiles and Mechanics</a></li>
+                            <li><a href="product-grids.html">Grocery</a></li>
+                            <li><a href="product-grids.html">Hardware and Plumbing</a></li>
+                            <li><a href="product-grids.html">Pharmacy</a></li>
+                            <li><a href="product-grids.html">Furniture and Carpentry</a></li>
+                            <!-- <li><a href="product-grids.html">top 100 offer</a></li>
 <li><a href="product-grids.html">sunglass</a></li>
 <li><a href="product-grids.html">watch</a></li>
 <li><a href="product-grids.html">man’s product</a></li>
@@ -170,26 +170,26 @@ Apple Watch Series 6</a></h4>
 <li><a class="dropdown-item" href="#">Grocery</a></li>
 <li><a class="dropdown-item" href="#">Electronics</a></li>
 <li><a class="dropdown-item" href="#">Hardware and Plumbing</a></li> -->
-</ul>
-</div>
-<!-- Search Box -->
-<div class="col-lg-5 col-md-8 col-sm-20 col-12" style="margin-left: 20%;margin-top:1%;margin-bottom:1%;">
+                        </ul>
+                    </div>
+                    <!-- Search Box -->
+                    <div class="col-lg-5 col-md-8 col-sm-20 col-12" style="margin-left: 20%;margin-top:1%;margin-bottom:1%;">
 
-<div class="main-menu-search">
+                        <div class="main-menu-search">
 
-<div class="navbar-search">
-<div class="search-input">
-<input type="text" placeholder="Search" >
-</div>
-<div class="search-btn">
-<button  style="background-color: #292b2c;"><i class="lni lni-search-alt"></i></button>
-</div>
-</div>
+                            <div class="navbar-search">
+                                <div class="search-input">
+                                    <input type="text" placeholder="Search">
+                                </div>
+                                <div class="search-btn">
+                                    <button style="background-color: #292b2c;"><i class="lni lni-search-alt"></i></button>
+                                </div>
+                            </div>
 
-</div>
+                        </div>
 
-</div>
-<!-- <div class="container">
+                    </div>
+                    <!-- <div class="container">
     <div class="row">    
         <div class="col-lg-5 col-md-7 col-xs-8 col-xs-offset-2" style="margin-left: 20%;margin-top:1%;margin-bottom:1%;">
             <div class="input-group">
@@ -217,20 +217,20 @@ Apple Watch Series 6</a></h4>
     </div>
 </div> -->
 
-<!-- End Search Box -->
+                    <!-- End Search Box -->
 
-<nav class="navbar navbar-expand-lg">
+                    <!-- <nav class="navbar navbar-expand-lg">
 <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 <span class="toggler-icon"></span>
 <span class="toggler-icon"></span>
 <span class="toggler-icon"></span>
-</button>
-<!-- <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+</button> -->
+                    <!-- <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
 <ul id="nav" class="navbar-nav ms-auto">
 <li class="nav-item">
 <a href="index.html" class="active" aria-label="Toggle navigation">Home</a>
 </li> -->
-<!-- <li class="nav-item">
+                    <!-- <li class="nav-item">
 <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#submenu-1-2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Pages</a>
 <ul class="sub-menu collapse" id="submenu-1-2">
 <li class="nav-item"><a href="about-us.html">About Us</a></li>
@@ -241,7 +241,7 @@ Apple Watch Series 6</a></h4>
 <li class="nav-item"><a href="404.html">404 Error</a></li>
 </ul>
 </li> -->
-<!-- <li class="nav-item">
+                    <!-- <li class="nav-item">
 <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#submenu-1-3" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Shop</a>
 <ul class="sub-menu collapse" id="submenu-1-3">
 <li class="nav-item"><a href="product-grids.html">Shop Grid</a></li>
@@ -266,32 +266,32 @@ Sibebar</a></li>
 </li> 
 </ul>
 </div>  -->
-</nav>
+                    </nav>
 
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-12">
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
 
-<div class="nav-social">
-<h5 class="title">Follow Us:</h5>
-<ul>
-<li>
-<a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-</li>
-<li>
-<a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
-</li>
-<li>
-<a href="javascript:void(0)"><i class="lni lni-instagram"></i></a>
-</li>
-<li>
-<a href="javascript:void(0)"><i class="lni lni-skype"></i></a>
-</li>
-</ul>
-</div>
+                <div class="nav-social">
+                    <h5 class="title">Follow Us:</h5>
+                    <ul>
+                        <li>
+                            <a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="lni lni-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="lni lni-skype"></i></a>
+                        </li>
+                    </ul>
+                </div>
 
-</div>
-</div>
-</div>
+            </div>
+        </div>
+    </div>
 
 </header>
